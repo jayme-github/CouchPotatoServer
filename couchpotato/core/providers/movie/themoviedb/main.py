f6582b956791afbb7e08a25d848b2ec69de47d41
Ruud <ruud@crashdummy.nl>
2012-12-19 08:01:35 +0100

    Catch urlerrors. closes #1154

--- couchpotato/core/providers/movie/themoviedb/main.py
+++ couchpotato/core/providers/movie/themoviedb/main.py
@@ -4,6 +4,7 @@ from couchpotato.core.logger import CPLog
 from couchpotato.core.providers.movie.base import MovieProvider
 from couchpotato.environment import Env
 from libs.themoviedb import tmdb
+import traceback
 
 log = CPLog(__name__)
 
@@ -63,7 +64,12 @@ class TheMovieDb(MovieProvider):
 
         if not results:
             log.debug('Searching for movie: %s', q)
-            raw = tmdb.search(search_string)
+
+            raw = None
+            try:
+                raw = tmdb.search(search_string)
+            except:
+                log.error('Failed searching TMDB for "%s": %s', (search_string, traceback.format_exc()))
 
             results = []
             if raw:
